03ef42d27b355fd2913e59371645b33dab19f678
Matthew Horridge <matthew.horridge@stanford.edu>
2021-08-13 13:22:38 -0700

    @ConditionalOnMissingBean

--- src/main/java/edu/stanford/protege/webprotege/common/WebProtegeCommonConfiguration.java
+++ src/main/java/edu/stanford/protege/webprotege/common/WebProtegeCommonConfiguration.java
@@ -1,14 +1,17 @@
 package edu.stanford.protege.webprotege.common;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.semanticweb.owlapi.model.OWLDataFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
 
 /**
  * Matthew Horridge
@@ -20,5 +23,11 @@ public class WebProtegeCommonConfiguration {
 
     public static void main(String[] args) {
         SpringApplication.run(WebProtegeCommonConfiguration.class, args);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    OWLDataFactory dataFactory() {
+        return new OWLDataFactoryImpl();
     }
 }
