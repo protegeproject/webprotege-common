1b638ae83bd7650f7ef45ca296cc0c29221f00d8
Matthew Horridge <matthew.horridge@stanford.edu>
2022-02-23 12:30:11 -0800

    Added ChangeRequestId

--- src/main/java/edu/stanford/protege/webprotege/common/ChangeRequestId.java
+++ src/main/java/edu/stanford/protege/webprotege/common/ChangeRequestId.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 import java.util.Objects;
+import java.util.UUID;
 
 public record ChangeRequestId(String id) implements ValueObject {
 
@@ -14,6 +15,14 @@ public record ChangeRequestId(String id) implements ValueObject {
     @JsonCreator
     public static ChangeRequestId valueOf(String id) {
         return new ChangeRequestId(id);
+    }
+
+    /**
+     * Generate a fresh {@link ChangeRequestId} using a randomly generated UUID
+     */
+    public static ChangeRequestId generate() {
+        var id = UUID.randomUUID().toString();
+        return valueOf(id);
     }
 
     @JsonValue
