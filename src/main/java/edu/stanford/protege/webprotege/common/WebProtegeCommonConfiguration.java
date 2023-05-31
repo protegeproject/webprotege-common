1eac67af612c78cbfd49809523fe3ce148e8a214
Matthew Horridge <matthew.horridge@stanford.edu>
2023-05-31 15:01:44 -0700

    Remove stale reference and dependencies to Kafka

--- src/main/java/edu/stanford/protege/webprotege/common/WebProtegeCommonConfiguration.java
+++ src/main/java/edu/stanford/protege/webprotege/common/WebProtegeCommonConfiguration.java
@@ -1,16 +1,10 @@
 package edu.stanford.protege.webprotege.common;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 import uk.ac.manchester.cs.owl.owlapi.OWLDataFactoryImpl;
 
 /**
